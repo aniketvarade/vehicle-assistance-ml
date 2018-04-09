5a18297a6f73e07f038754158503cf52a753467f
Aniket Varade <aniket.varade@gmail.com>
2018-04-10 00:04:53 +0530

    location.href in onclick method

--- routes/users.js
+++ routes/users.js
@@ -15,11 +15,6 @@ router.get('/login', function(req, res){
 	res.render('login');
 });
 
-// About Us
-router.get('/about', function(req, res){
-	res.render('about');
-});
-
 // Register User
 router.post('/register', function(req, res){
 	var name = req.body.name;
