fa613c4f0d60b6587e08db30597d9d9f45fb29f3
Aniket Varade <aniket.varade@gmail.com>
2018-04-13 00:08:13 +0530

    user exists console, database changes, welcome user, footer

--- models/user.js
+++ models/user.js
@@ -7,21 +7,25 @@ var SchemaTypes = mongoose.SchemaTypes;
 var UserSchema = mongoose.Schema({
 	username: {
 		type: String,
-		index:true
+		index: true
 	},
 	password: {
 		type: String
 	},
 	email: {
 		type: String,
-		unique: true
+		unique: true,
+		dropDups: true
 	},
 	number: {
 		type: Number,
-		unique: true
+		unique: true,
+		dropDups: true
 	},
 	name: {
-		type: String
+		type: String/*,
+		unique:true,
+		dropDups:true*/
 	},
 	model: {
 		type: String
@@ -63,6 +67,34 @@ var UserSchema = mongoose.Schema({
 });
 
 var User = module.exports = mongoose.model('User', UserSchema);
+
+UserSchema.pre('save', function (next) {
+    var self = this;
+    User.find({email : self.email}, function (err, docs) {
+        if (!docs.length){
+            next();
+        }else{   
+            console.log('email exists: ',self.email);
+            next(new Error("email exists!"));
+        }
+    });
+}) ;
+
+/*UserSchema.pre('save', function(next, done) {
+    var self = this;
+    mongoose.models["User"].findOne({username: self.username}, function(err, user) {
+        if(err) {
+            done(err);
+        } else if(user) {            
+            if (user._id.equals(self._id)) return next(); // If id's are equal, then we don't care about false dupe username error because its the same user!
+
+            self.invalidate('username', 'Sorry but this username is already taken');
+            done(new Error('Sorry but this username is already taken'));
+        } else {
+            next();
+        }
+    });
+});*/
 
 module.exports.createUser = function(newUser, callback){
 	bcrypt.genSalt(10, function(err, salt) {
--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
-	res.render('index');
+	res.render('index', {username: req.user.name});
 });
 
 // Update
 router.get('/update', ensureAuthenticated, function(req, res){
-	res.render('update');
+	res.render('update', {username: req.user.name});
 });
 
 // About
 router.get('/about', ensureAuthenticated, function(req, res){
-	res.render('about');
+	var user = req.user.email;
+	console.log(user);
+	res.render('about', {username: req.user.name});
 });
 
 // Pitstop
 router.get('/pitstop', ensureAuthenticated, function(req, res){
-	res.render('pitstop');
+	res.render('pitstop', {username: req.user.name});
 });
 
 // NeedAssistance
 router.get('/needassistance', ensureAuthenticated, function(req, res){
-	res.render('needassistance');
+	res.render('needassistance', {username: req.user.name});
 });
 
 // Result
 router.get('/result', ensureAuthenticated, function(req, res){
-	res.render('result');
+	res.render('result', {username: req.user.name});
 });
 
 // Other Result
 router.get('/otherresult', ensureAuthenticated, function(req, res){
-	res.render('otherresult');
+	res.render('otherresult', {username: req.user.name});
 });
 
 // Thank you
 router.get('/thankyou', ensureAuthenticated, function(req, res){
-	res.render('thankyou');
+	res.render('thankyou', {username: req.user.name});
 });
 
 // Update post
 router.post('/update', ensureAuthenticated, function(req, res){
 	var model = req.body.selectpicker;
+	var tire = req.body.tire;
+	var coolant = req.body.coolant;
+	var oil = req.body.oil;
+	var name = req.user.name;
+	
+	User.findByIdAndUpdate
 
 	res.render('update');
 });
